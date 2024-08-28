811c97238113782dce3c3d88ac9250847af43216
i.molochnikov <i.molochnikov@scloud.ru>
2024-08-28 14:16:32 +0300

    тест

---
+++ www/app/Jobs/ProcessEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class ProcessEvent implements ShouldQueue
+{
+    use Queueable, Dispatchable, InteractsWithQueue,  SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        DB::table('test_works')
+            ->insert([
+                'work_name' => '1232123'
+            ]);
+    }
+}
